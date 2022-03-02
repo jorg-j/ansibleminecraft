ac200ee03c78268b55b2b24f8b6f0e61580a4a59
jorg-j <47870433+jorg-j@users.noreply.github.com>
2022-03-02 19:57:21 +1100

    playbook

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## Install ansible and download + run playbook
+## curl -o- https://raw.githubusercontent.com/jorg-j/ansibleminecraft/main/install.sh | bash
 
 USER=$(whoami)
 
@@ -10,3 +11,6 @@ sudo apt upgrade -y
 sudo apt install ansible git -y
 
 wget -O /home/$USER/Downloads/mcraft_play.yml https://raw.githubusercontent.com/jorg-j/ansibleminecraft/main/mcraft_play.yml
+
+
+ansible-playbook /home/pi/Downloads/mcraft_play.yml
